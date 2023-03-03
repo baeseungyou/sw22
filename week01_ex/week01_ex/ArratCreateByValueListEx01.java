89564837452853e74454c73a9840b62d40f1be29
baeseungyou <101550023+baeseungyou@users.noreply.github.com>
2023-03-04 00:06:50 +0900

    Add files via upload

---
+++ week01_ex/week01_ex/ArratCreateByValueListEx01.java
@@ -0,0 +1,5 @@
+package week01_ex;
+
+public class ArratCreateByValueListEx01 {
+
+}
---
+++ week01_ex/week01_ex/Ex1.java
@@ -0,0 +1,10 @@
+package week01_ex;
+
+public class Ex1 {
+
+	public static void main(String[] args) {
+		System.out.println("test");
+
+	}
+
+}
